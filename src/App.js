a10588fbc43035c2478fab65107c1acd8d8b1991
Mongithabet <stechnopc@yahoo.fr>
2021-10-29 18:26:09 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-
+import Profile from './components/Profile'
+import { UserContext } from './components/MyContext'
+import { ColorContext } from './components/MyContext'
 export default class App extends Component {
 
 
   state = {
-    user
+    user: {
+      name: 'Lisa',
+      age: 8
+    }
   }
   render() {
     return (
-      <div>
-        
-      </div>
+      <UserContext.Provider value={this.state.user}>
+        <ColorContext.Provider value={'red'} >
+        <Profile />
+        </ColorContext.Provider>
+      </UserContext.Provider>
     )
-  
+  }
+}
--- src/components/ContentData.js
+++ src/components/ContentData.js
@@ -1,12 +1,47 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { ColorContext, UserContext } from './MyContext'
 export const ContentData = () => {
+    const user =useContext(UserContext)
+    const color =useContext(ColorContext)
+
+
     return (
-        <div>
-            <ul>
-                <li> Nom: </li>
-                <li> Age: </li>
-            </ul>
+        <div style={{color: color}}>
+                   <ul>
+                        <li> Nom: {user.name} </li>
+                        <li> Age: {user.age} </li>
+                        <li> Couleur: {color}  </li>
+                    </ul>
         </div>
     )
 }
+{/*---------------Methode Classique------------------ */}
+      
+{/*  <UserContext.Consumer>
+           
+            {
+                
+                
+                user =>{
+                   return(
+                    <ColorContext.Consumer>
+                    {
+                        color =>{
+                            return(
+                <div style={{color: color}}>
+                    <ul>
+                        <li> Nom: {user.name} </li>
+                        <li> Age: {user.age} </li>
+                        <li> Couleur: {color}  </li>
+                    </ul>
+                </div>  )
+                        }
+                    }
+                    </ColorContext.Consumer> 
+                   
+                   )
+                }
+                
+            }
+                
+        </UserContext.Consumer> */}
